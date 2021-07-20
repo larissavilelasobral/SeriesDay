d6b086f21d6e44ade16d883fc48775defe9989e8
Larissa Vilela Sobral <larissavilelasobral@gmail.com>
2021-07-20 18:04:41 -0300

    função criar conta

--- src/main.js
+++ src/main.js
@@ -1,46 +1,71 @@
 // Este é o ponto de entrada da sua aplicação
 
+// const { markAsUntransferable } = require("node:worker_threads");
+
 // import { myFunction } from './lib/index.js';
 
 // myFunction();
 
-const email = 'patricia.argetlam@gmail.com';
-const password = '123456';
 
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('Confpassword');
+const newUser = document.getElementById('nonUser');
+const signInButton = document.getElementById('signin-button');
 
-//Login de novos usuários
+const signUpButton = document.getElementById('signup-button');
+const signOutButton = document.getElementById('signout-button');
 
-firebase
-  .auth()
-  .createUserWithEmailAndPassword(email, password)
+// login usuarios existentes
+signInButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  // eslint-disable-next-line max-len
+firebase.auth().signInWithEmailAndPassword(email.value, password.value)
   .then((userCredential) => {
     // Signed in
-    const user = userCredential.user;
-    console.log('deu bom', user)
+    var user = userCredential.user;
+    alert(`Login feito pelo Email: ${email.value}`)
     // ...
   })
   .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-
-    console.log('deu ruim', errorCode, errorMessage);
-    // ..
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(`Não há registro de usuário correspondente a este Email`)
   });
-
-
-//Login de usuários existentes
-
-  firebase.auth().signInWithEmailAndPassword(email, password)
-  .then((userCredential) => {
+});
+// criar novo Login de usuários
+signUpButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  // eslint-disable-next-line max-len
+  firebase.auth().createUserWithEmailAndPassword(email.value, password.value).then((userCredential) => {
     // Signed in
     const user = userCredential.user;
-
-    console.log('logou!')
-    // ...
+    console.log('deu bom', user);
   })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
+    .catch((error) => {
+      newUser.innerHTML = error.message;
+      console.log('deu ruim');
+    });
+});
 
-    console.log('não logou')
-  });
+// sair da conta do usuario
+signOutButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  firebase.auth().signOut();
+  location.reload();
+});
+
+// firebase.auth().onAuthStateChanged((user) => {
+//     if (user) {
+//        const email = user.email;
+//         console.log(`Usuario ativo ${email}`);
+//       // User is signed in, see docs for a list of available properties
+//       // https://firebase.google.com/docs/reference/js/firebase.User
+//       var uid = user.uid;
+//       // ...
+//     } else {
+//       console.log(`nenhum usuario ativo`);
+//       // User is signed out
+//       // ...
+//     }
+// });
